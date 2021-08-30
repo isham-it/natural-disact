ab7a4b54d4c50159f6cae7eb5dc03d3031b74733
Alex <alexandre.deboisgency@gmail.com>
2021-08-30 12:05:17 +0200

    catastrophe_sos_db

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'username',
+        'first_name',
+        'last_name',
         'email',
+        'city',
         'password',
+        'phone',
     ];
 
     /**
