b6ccfb306b64a4f1e20bc4c6b8648c5a524a5a23
isham-it <85164015+isham-it@users.noreply.github.com>
2021-08-31 15:26:14 +0200

    new

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>Footer</title>
+ <link rel="stylesheet" href="{{ URL::asset('public/css/footer.css') }}">
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+     integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+     crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+ </head>
+
+<body>
+    <footer>
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fab fa-tiktok"></i>
+                </a>
+            </li>
+        </ul>
+
+    </footer>
+</body>
+
+
+
+
+</body>
+</html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,17 +1,27 @@
-    <link rel="stylesheet" href="{{ URL::asset('public/css/nav.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>navbar</title>
+ <link rel="stylesheet" href="{{ URL::asset('public/css/nav.css') }}">
+ <link rel="stylesheet" href="{{ URL::asset('public/css/main.css') }}">
 
-<nav class="navbar">
-    <ul>
-        <li><a href="{{ url('home') }}">Home page</a></li>
-        <li><a href="{{ url('about') }}">About us</a></li>
-        <li><a href="{{ url('contact') }}">contact us </a></li>
-        <li><a href="{{ url('register') }}">Register</a></li>
-        <li><a href="{{ url('login') }}">Login</a></li>
-        <li><a href="{{ url('event') }}">Event page</a></li>
-    </ul>
-</nav>
+ </head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="{{ url('home') }}">Home page</a></li>
+            <li><a href="{{ url('about') }}">About us</a></li>
+            <li><a href="{{ url('contact') }}">contact us </a></li>
+            <li><a href="{{ url('register') }}">Register</a></li>
+            <li><a href="{{ url('login') }}">Login</a></li>
+            <li><a href="{{ url('event') }}">Event page</a></li>
+        </ul>
+    </nav>
+    
 
 
+</body>
+</html>
 
 
