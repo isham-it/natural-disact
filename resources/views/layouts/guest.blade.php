6844be7d67840b1c29c96d269243bc1eb7e542a4
Alex <alexandre.deboisgency@gmail.com>
2021-09-03 15:09:40 +0200

    Login Register Form and Access DB

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,13 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         
-        <--<link rel="stylesheet" href="{{ asset('css/main.css') }} ">
+        <link rel="stylesheet" href="{{ asset('css/main.css') }} ">
         
         <link rel="stylesheet" href="{{ asset('css/register.css') }} ">
 
